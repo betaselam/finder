7387c075bb5f34d2168618bee2b960f1e1c05ceb
khurramlone <khurram.lone@gmail.com>
2013-10-09 16:21:28 -0400

    Update globals.js
    changed handler to '/services'

--- js/app/config/environment/globals.js
+++ js/app/config/environment/globals.js
@@ -3,7 +3,7 @@
 //window.gblEnvironment = 'production';
 window.gblEnvironment = 'dev';
 window.gblAssetsURL = "assets.hhs.gov/resources/";
-window.gblServiceHandler = "handler/ServiceHandler.ashx";
+window.gblServiceHandler = "/services";
 window.gblCountiesForZipServiceURL = "https://rbis2gateway.insuranceoversight.hhs.gov/RBIS-API/v5.6/REST/getCountiesForZip";
 window.gblPlansServiceURL = "https://hiosvalgateway.cms.gov/RBIS-API/v5.6/REST/getPlans";
 window.gblProductsServiceURL = "https://hiosvalgateway.cms.gov/RBIS-API/v5.6/REST/getProducts";
