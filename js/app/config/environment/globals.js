4b46d1087ac086a41d46d530df831b93f3e9f520
khurramlone <khurram.lone@gmail.com>
2013-10-15 13:01:52 -0400

    Update globals.js
    added new entries for proxy

--- js/app/config/environment/globals.js
+++ js/app/config/environment/globals.js
@@ -1,14 +1,19 @@
-﻿// Add ONLY global variables that change from one environment to another
+// Add ONLY global variables that change from one environment to another
 // ---------------------------------------------------------------------
 //window.gblEnvironment = 'production';
 window.gblEnvironment = 'dev';
 window.gblAssetsURL = "assets.hhs.gov/resources/";
-window.gblServiceHandler = "/services";
+window.gblServiceHandler = "handler/ServiceHandler.ashx";
+window.gblCountyServiceHandler = "http://hc-finder.cgifederal.com/services/getCountiesForZip";
 window.gblCountiesForZipServiceURL = "https://rbis2gateway.insuranceoversight.hhs.gov/RBIS-API/v5.6/REST/getCountiesForZip";
-window.gblPlansServiceURL = "https://hiosvalgateway.cms.gov/RBIS-API/v5.6/REST/getPlans";
-window.gblProductsServiceURL = "https://hiosvalgateway.cms.gov/RBIS-API/v5.6/REST/getProducts";
-window.gblPlanBenefitsServiceURL = "https://hiosvalgateway.cms.gov/RBIS-API/v5.6/REST/getPlanBenefits";
-window.gblProductBenefitsServiceURL = "https://hiosvalgateway.cms.gov/RBIS-API/v5.6/REST/getProductBenefits";
+window.gblPlansServiceHandler = "http://hc-finder.cgifederal.com/services/getPlans";
+window.gblPlansServiceURL = "https://rbis2gateway.insuranceoversight.hhs.gov/RBIS-API/REST/getPlans";
+window.gblProductsServiceHandler = "http://hc-finder.cgifederal.com/services/getProducts";
+window.gblProductsServiceURL = "https://rbis2gateway.insuranceoversight.hhs.gov/RBIS-API/REST/getProducts";
+window.gblPlanBenefitsServiceHandler = "http://hc-finder.cgifederal.com/services/getPlanBenefits";
+window.gblPlanBenefitsServiceURL = "https://rbis2gateway.insuranceoversight.hhs.gov/RBIS-API/v5.6/REST/getPlanBenefits";
+window.gblProductBenefitsServiceHandler = "http://hc-finder.cgifederal.com/services/getProductBenefits";
+window.gblProductBenefitsServiceURL = "https://rbis2gateway.insuranceoversight.hhs.gov/RBIS-API/v5.6/REST/getProductBenefits";
 
 
 
